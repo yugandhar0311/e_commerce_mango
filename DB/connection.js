9020c309e17242df77dac3d8a845caf494de303e
yugandhar0311 <82320736+yugandhar0311@users.noreply.github.com>
2021-07-02 20:22:49 +0530

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const URI = "mongodb+srv://sajid_01:sajid_01@cluster0.lsme9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const URI = "PASTE_MONGO_URI"
 
 const connectDB = async() => {
     await mongoose.connect(URI, {
